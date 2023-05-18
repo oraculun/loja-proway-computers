9b260b9e967f71881e0b8c2d4900402937e88d63
Luciano Passarelli <passarellidev@gmail.com>
2023-05-18 10:35:25 -0300

    concluido barra de pesquisa 6.0
    Barra de pesquisa funcionando

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduto } from '../produtos';
 import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
@@ -11,11 +12,22 @@ export class ProdutosComponent implements OnInit{
   produtos: IProduto[] | undefined;
 
   constructor(
-    private ProdutosService: ProdutosService
+    private ProdutosService: ProdutosService,
+    private route: ActivatedRoute
   ){}
 
   ngOnInit(): void {
-    this.produtos = this.ProdutosService.getAll();
+    const produtos = this.ProdutosService.getAll();
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+
+      if(descricao){
+        this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+        return
+      }
+
+      this.produtos = produtos;
+    });
   }
 
 
